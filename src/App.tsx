c1dbfe2d681d3ffa52df064c127c2b7d6b57f794
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-08-03 16:45:32 -0500

    typescript quiz start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+//components
+import QuestionCard from './Components/QuestionCard'
+import { fetchQuizQuestions } from './API';
+//types
+import { Difficulty } from './API';
+
+const TOTAL_QUESTIONS=10
 
 function App() {
+  //------------------------useState-------------------------//
+  const [loading, setLoading]= useState(false);
+  const [questions, setQuestions]=useState([]);
+  const [number, setNumber]= useState(0);
+  const [userAnswers, setUserAnswers]=useState([]);
+  const [score, setScore]=useState(0);
+  const [gameOver, setGameOver]=useState(true);
+  //----------------------------------------------------------//
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+  const startQuiz = async ()=>{
+
+  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>)=>{
+
+  }
+
+  const nextQuestion = () =>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz</h1>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      {/* <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions ={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
